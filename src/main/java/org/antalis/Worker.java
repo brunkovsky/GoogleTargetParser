74e419fb44571ba56e6ab161bc5a2504e73de50d
brunkovskyi <brunkovskyi@ngoar.net>
2018-07-12 11:17:58 +0300

    remove txt writer. add condition for html files only

--- src/main/java/org/antalis/Worker.java
+++ src/main/java/org/antalis/Worker.java
@@ -12,7 +12,7 @@ import java.util.logging.Logger;
 
 class Worker {
     private Writer excelWriter;
-    private Writer txtWriter;
+//    private Writer txtWriter;
     private String mainDirectory;
     private static final String PREFIX = "trc_";
     private static final Logger logger = Logger.getLogger(Worker.class.getName());
@@ -21,13 +21,13 @@ class Worker {
         this.mainDirectory = mainDirectory;
         String timeStamp = new SimpleDateFormat("_yyyyMMdd_HHmmss").format(new Date());
         excelWriter = new ExcelWriter(mainDirectory + timeStamp + ".xlsx");
-        txtWriter = new TxtWriter(mainDirectory + timeStamp + ".txt");
+//        txtWriter = new TxtWriter(mainDirectory + timeStamp + ".txt");
     }
 
     void execute() throws IOException {
         processFilesFromFolder(new File(mainDirectory));
         excelWriter.close();
-        txtWriter.close();
+//        txtWriter.close();
         logger.info("done");
     }
 
@@ -47,15 +47,17 @@ class Worker {
     }
 
     private void manageResult(File entry) throws IOException {
-        Document doc = Jsoup.parse(entry, "utf-8");
-        Elements trc_ = doc.select("[class^=" + PREFIX + "]");
-        String title = doc.title();
-        for (Element element: trc_) {
-            String path = entry.getPath().split("/sites/")[1].split(".html")[0];
-            String className = element.className().split(" ")[0];
-            String description = element.text();
-            excelWriter.write(path, title, className, description);
-            txtWriter.write(path, title, className, description);
+        if (entry.getName().endsWith(".html")) {
+            Document doc = Jsoup.parse(entry, "utf-8");
+            Elements trc_ = doc.select("[class^=" + PREFIX + "]");
+            String title = doc.title();
+            for (Element element : trc_) {
+                String path = entry.getPath();
+                String className = element.className().split(" ")[0];
+                String description = element.text();
+                excelWriter.write(path, title, className, description);
+//            txtWriter.write(path, title, className, description);
+            }
         }
     }
 }
